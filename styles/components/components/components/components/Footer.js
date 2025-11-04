625d250f9fddc2cda183dd4a387604d64ae58d86
kyang699-collab <kyang699@stpaul.k12.mn.us>
2025-11-04 12:38:59 -0600

    Add Footer component with copyright notice

---
+++ styles/components/components/components/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      &copy; {new Date().getFullYear()} now.gg clone by AI. Not affiliated with now.gg.
+    </footer>
+  );
+}
