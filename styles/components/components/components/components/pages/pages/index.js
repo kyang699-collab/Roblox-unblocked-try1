abade1f875db6cc6b4018064acd25989959c3584
kyang699-collab <kyang699@stpaul.k12.mn.us>
2025-11-04 12:39:59 -0600

    Add Home component with layout structure

---
+++ styles/components/components/components/components/pages/pages/index.js
@@ -0,0 +1,15 @@
+import HeroSection from "../components/HeroSection";
+import Features from "../components/Features";
+import GameGrid from "../components/GameGrid";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+      <Features />
+      <GameGrid />
+      <Footer />
+    </main>
+  );
+}
